3ed8bc0f7459e6522e67dfe6a58cbe96a222664e
krystian <krystian.zawistowski4@gmail.com>
2020-02-25 14:36:40 +0100

    Choose arima model

--- arima/model_selection.py
+++ arima/model_selection.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 from arima import get_forecast
@@ -6,31 +5,55 @@ from arima.arima_model import fit_arma
 
 
 def aic(var_eps, deg_of_freedom, n_points):
-    return 2 * deg_of_freedom + 2*n_points*np.log(var_eps)
+    return 2 * deg_of_freedom + 2 * n_points * np.log(var_eps)
 
 
 def bic(var_eps, deg_of_freedom, n_points):
-    return np.log(n_points)* deg_of_freedom +2 *n_points* np.log(var_eps)
+    return np.log(n_points) * deg_of_freedom + 2 * n_points * np.log(var_eps)
 
 
 def choose_arma_model(data, max_ar_lags=3, max_ma_lags=3, criterion='bic'):
     results = {}
-    if isinstance(criterion,str):
+    if isinstance(criterion, str):
         criterion = {
             'bic': bic, 'aic': aic
         }[criterion]
-    for ar_lag in range(max_ar_lags+1):
-        for ma_lag in range(max_ma_lags+1):
-
-            ar_params, ma_params= fit_arma(data, ar_lag, ma_lag )
+    for ar_lag in range(max_ar_lags + 1):
+        for ma_lag in range(max_ma_lags + 1):
+            ar_params, ma_params = fit_arma(data, ar_lag, ma_lag)
             forecast = get_forecast(data, ar_params, ma_params)
-            results[(ar_lag,ma_lag)]={
+            results[(ar_lag, ma_lag)] = {
                 'ar_params': ar_params,
-                'ma_params':ma_params,
-                'criterion':criterion(np.var(data - forecast), ar_lag+ma_lag, len(data)),
+                'ma_params': ma_params,
+                'criterion': criterion(np.var(data - forecast), ar_lag + ma_lag, len(data)),
                 'eps_variance': np.var(data - forecast)
             }
     # results = sorted(results, key = lambda x: x['criterion'])
-    return results, sorted([r for r in results.values() if not np.isnan(r['criterion'])], key=lambda x: x['criterion'])[0]
+    return results, sorted([r for r in results.values() if not np.isnan(r['criterion'])], key=lambda x: x['criterion'])[
+        0]
 
 
+def choose_arima_model(data, max_ar_lags=3, max_ma_lags=3, max_i=0, criterion='bic'):
+    results = {}
+    if isinstance(criterion, str):
+        criterion = {
+            'bic': bic, 'aic': aic
+        }[criterion]
+    for ar_lag in range(max_ar_lags + 1):
+        for ma_lag in range(max_ma_lags + 1):
+            for i in range(max_i + 1):
+                data2 = np.array(data)
+                for _ in range(i):
+                    data2 = np.diff(data2)
+                ar_params, ma_params = fit_arma(data, ar_lag, ma_lag)
+                forecast = get_forecast(data, ar_params, ma_params)
+                results[(ar_lag, ma_lag, i)] = {
+                    'ar_params': ar_params,
+                    'ma_params': ma_params,
+                    'i_deg': i,
+                    'criterion': criterion(np.var(data - forecast), ar_lag + ma_lag, len(data)),
+                    'eps_variance': np.var(data - forecast)
+                }
+    # results = sorted(results, key = lambda x: x['criterion'])
+    return results, sorted([r for r in results.values() if not np.isnan(r['criterion'])], key=lambda x: x['criterion'])[
+        0]
--- test/mc/test_model_selection.py
+++ test/mc/test_model_selection.py
@@ -9,7 +9,9 @@ def test_arma_model_selection_reproduces_original():
     data,eps = generate_process(ar_terms, ma_terms,10000)
     forecast = get_forecast(data, ar_terms, ma_terms)
     print("Variance of data", np.var(data))
-    results, model=choose_arma_model(data, 5, 5)
+    results, model=choose_arma_model(data, 8, 8)
+    print(model)
+    print(results)
     assert len(model['ar_params'] == 2)
     assert len(model['ma_params'] == 2)
     assert np.var(data-forecast) > model["eps_variance"]
